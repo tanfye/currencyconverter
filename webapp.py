3b23c4796a418f32925ac8dcca9da2dc2ed7f8ef
m_zuzuleac18 <m_zuzuleac18@cranford.hounslow.sch.uk>
2024-03-14 11:29:33 +0000

    print line

--- webapp.py
+++ webapp.py
@@ -1,2 +1,12 @@
-print("hello")
-print("bye")
+import streamlit as st
+
+st.title("Currency Converter")
+st.text_input("Enter amount")
+st.radio("Select currency:", ["GBP to USD", "GBP to JPY", "USD to GBP", "JPY to GBP"])
+
+col1, col2 = st.columns([1, 2])
+
+with col1:
+    st.text("Converted amount: ")
+with col2:
+    st.text("908")
