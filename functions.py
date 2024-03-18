5589815ef45aece0c1c97f527c65b1e94dd6fe21
m_zuzuleac18 <m_zuzuleac18@cranford.hounslow.sch.uk>
2024-03-18 13:29:02 +0000

    print line

--- functions.py
+++ functions.py
@@ -4,10 +4,10 @@ def currency_converter(amount, currency):
     elif currency == "GBP to JPY":
         rate = 190
     elif currency == "USD to GBP":
-        rate = 1.27
+        rate = 0.79
     elif currency == "JPY to GBP":
         rate = 0.0053
 
-    new_amount = amount * rate
+    new_amount = rount(amount * rate, 2)
     return new_amount
 
--- webapp.py
+++ webapp.py
@@ -1,12 +1,19 @@
 import streamlit as st
+from functions import currency_converter
 
 st.title("Currency Converter")
-st.text_input("Enter amount")
-st.radio("Select currency:", ["GBP to USD", "GBP to JPY", "USD to GBP", "JPY to GBP"])
+amount = st.text_input("Enter amount")
+currency = st.radio("Select currency:", ["GBP to USD", "GBP to JPY", "USD to GBP", "JPY to GBP"])
 
-col1, col2 = st.columns([1, 2])
 
-with col1:
-    st.text("Converted amount: ")
-with col2:
-    st.text("908")
+if amount:
+    try:
+      new_amount = currency_converter(float(amount), currency)
+      col1, col2 = st.columns([1, 2])
+      with col1:
+        st.text("Converted amount: ")
+      with col2:
+        st.text(new_amount)
+
+    except ValueError:
+        st.info("Amount must be a number")
